7a2dd8d859fabad8804b258f558bc10257587b20
zengzz01 <zengzz01@mingyuanyun.com>
2022-09-14 14:03:08 +0800

    ca email address

--- core/certificate.go
+++ core/certificate.go
@@ -103,7 +103,7 @@ func template(host string, expireDays int) *x509.Certificate {
 		BasicConstraintsValid: true,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageDataEncipherment | x509.KeyUsageKeyEncipherment,
-		EmailAddresses:        []string{"zenzz01@mingyuanyun.com"},
+		EmailAddresses:        []string{"514551226@qq.com"},
 	}
 	hosts := strings.Split(host, ",")
 	for _, item := range hosts {
